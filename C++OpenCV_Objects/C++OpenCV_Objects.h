02d00bd27d9a57a8e52ae122583668f73614492d
kujin <1604323361@qq.com>
2020-11-05 18:03:04 +0800

    添加项目文件。

---
+++ C++OpenCV_Objects/C++OpenCV_Objects.h
@@ -0,0 +1,32 @@
+﻿
+// C++OpenCV_Objects.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CCOpenCVObjectsApp:
+// 有关此类的实现，请参阅 C++OpenCV_Objects.cpp
+//
+
+class CCOpenCVObjectsApp : public CWinApp
+{
+public:
+	CCOpenCVObjectsApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCOpenCVObjectsApp theApp;
---
+++ C++OpenCV_Objects/C++OpenCV_ObjectsDlg.h
@@ -0,0 +1,34 @@
+﻿
+// C++OpenCV_ObjectsDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CCOpenCVObjectsDlg 对话框
+class CCOpenCVObjectsDlg : public CDialogEx
+{
+// 构造
+public:
+	CCOpenCVObjectsDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_COPENCV_OBJECTS_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
